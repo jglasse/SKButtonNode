616b861cdea3a9b7a04a50da4965d81fc533dec4
Jeff Glasse <jglasse@gmail.com>
2017-02-06 07:22:00 -0800

    Swift 3 syntax conversion

--- SKButtonNode.swift
+++ SKButtonNode.swift
@@ -1,149 +1,153 @@
 //SKButtonNode.swift
 //Using contributions from: http://stackoverflow.com/questions/19082202/setting-up-buttons-in-skscene
 
+// updated to Swift 3 syntax by JG 1/13/17
+
 import Foundation
 import SpriteKit
 
 class SKButtonNode: SKSpriteNode {
-    
-    enum SKButtonNodeActionType: Int {
-        case TouchUpInside = 1,
-        TouchDown, TouchUp
-    }
-    
-    var isEnabled: Bool = true {
-        didSet {
-            if (disabledTexture != nil) {
-                texture = isEnabled ? defaultTexture : disabledTexture
-            }
+
+enum SKButtonNodeActionType: Int {
+    case TouchUpInside = 1,
+    TouchDown, TouchUp
+}
+
+var isEnabled: Bool = true {
+    didSet {
+        if (disabledTexture != nil) {
+            texture = isEnabled ? defaultTexture : disabledTexture
         }
     }
-    var isSelected: Bool = false {
-        didSet {
-            texture = isSelected ? selectedTexture : defaultTexture
-        }
+}
+var isSelected: Bool = false {
+    didSet {
+        texture = isSelected ? selectedTexture : defaultTexture
+    }
+}
+
+var defaultTexture: SKTexture
+var selectedTexture: SKTexture
+var label: SKLabelNode
+
+required init(coder: NSCoder) {
+    fatalError("NSCoding not supported")
+}
+
+init(normalTexture defaultTexture: SKTexture!, selectedTexture:SKTexture!, disabledTexture: SKTexture?) {
+    
+    self.defaultTexture = defaultTexture
+    self.selectedTexture = selectedTexture
+    self.disabledTexture = disabledTexture
+    self.label = SKLabelNode(fontNamed: "Helvetica")
+    
+    super.init(texture: defaultTexture, color: UIColor.white, size: defaultTexture.size())
+    
+    isUserInteractionEnabled = true
+    
+    //Creating and adding a blank label, centered on the button
+    self.label.verticalAlignmentMode = SKLabelVerticalAlignmentMode.center;
+    self.label.horizontalAlignmentMode = SKLabelHorizontalAlignmentMode.center;
+    
+    //make sure label is above button
+    self.label.zPosition = self.zPosition + 1
+    addChild(self.label)
+    
+    // Adding this node as an empty layer. Without it the touch functions are not being called
+    // The reason for this is unknown when this was implemented...?
+    let bugFixLayerNode = SKSpriteNode(texture: nil, color: UIColor.clear, size: defaultTexture.size())
+    bugFixLayerNode.position = self.position
+    addChild(bugFixLayerNode)
+    
+}
+
+/**
+ * Taking a target object and adding an action that is triggered by a button event.
+ */
+func setButtonAction(target: AnyObject, triggerEvent event:SKButtonNodeActionType, action:Selector) {
+    
+    switch (event) {
+    case .TouchUpInside:
+        targetTouchUpInside = target
+        actionTouchUpInside = action
+    case .TouchDown:
+        targetTouchDown = target
+        actionTouchDown = action
+    case .TouchUp:
+        targetTouchUp = target
+        actionTouchUp = action
     }
     
-    var defaultTexture: SKTexture
-    var selectedTexture: SKTexture
-    var label: SKLabelNode
+}
+
+/*
+ New function for setting text. Calling function multiple times does
+ not create a ton of new labels, just updates existing label.
+ You can set the title, font type and font size with this function
+ */
+
+func setButtonLabel(title: NSString, font: String, fontSize: CGFloat) {
+    self.label.text = title as String
+    self.label.fontSize = fontSize
+    self.label.fontName = font
+}
+
+var disabledTexture: SKTexture?
+var actionTouchUpInside: Selector?
+var actionTouchUp: Selector?
+var actionTouchDown: Selector?
+weak var targetTouchUpInside: AnyObject?
+weak var targetTouchUp: AnyObject?
+weak var targetTouchDown: AnyObject?
+
+
+override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    if (!isEnabled) {
+        return
+    }
+    isSelected = true
     
-    required init(coder: NSCoder) {
-        fatalError("NSCoding not supported")
+    if (targetTouchDown != nil && targetTouchDown!.responds(to: actionTouchDown!))
+    {
+        UIApplication.shared.sendAction(actionTouchDown!, to: targetTouchDown, from: self, for: nil)
+    }
+}
+
+override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+    
+    if (!isEnabled) {
+        return
     }
     
-    init(normalTexture defaultTexture: SKTexture!, selectedTexture:SKTexture!, disabledTexture: SKTexture?) {
-        
-        self.defaultTexture = defaultTexture
-        self.selectedTexture = selectedTexture
-        self.disabledTexture = disabledTexture
-        self.label = SKLabelNode(fontNamed: "Helvetica");
-        
-        super.init(texture: defaultTexture, color: UIColor.whiteColor(), size: defaultTexture.size())
-        userInteractionEnabled = true
-        
-        //Creating and adding a blank label, centered on the button
-        self.label.verticalAlignmentMode = SKLabelVerticalAlignmentMode.Center;
-        self.label.horizontalAlignmentMode = SKLabelHorizontalAlignmentMode.Center;
-        
-        //make sure label is above button
-        self.label.zPosition = self.zPosition + 1
-        addChild(self.label)
-        
-        // Adding this node as an empty layer. Without it the touch functions are not being called
-        // The reason for this is unknown when this was implemented...?
-        let bugFixLayerNode = SKSpriteNode(texture: nil, color: UIColor.clearColor(), size: defaultTexture.size())
-        bugFixLayerNode.position = self.position
-        addChild(bugFixLayerNode)
-        
-    }
-    
-    /**
-     * Taking a target object and adding an action that is triggered by a button event.
-     */
-    func setButtonAction(target: AnyObject, triggerEvent event:SKButtonNodeActionType, action:Selector) {
-        
-        switch (event) {
-        case .TouchUpInside:
-            targetTouchUpInside = target
-            actionTouchUpInside = action
-        case .TouchDown:
-            targetTouchDown = target
-            actionTouchDown = action
-        case .TouchUp:
-            targetTouchUp = target
-            actionTouchUp = action
-        }
-        
-    }
-    
-    /*
-    New function for setting text. Calling function multiple times does
-    not create a ton of new labels, just updates existing label.
-    You can set the title, font type and font size with this function
-    */
-    
-    func setButtonLabel(title: NSString, font: String, fontSize: CGFloat) {
-        self.label.text = title as String
-        self.label.fontSize = fontSize
-        self.label.fontName = font
-    }
-    
-    var disabledTexture: SKTexture?
-    var actionTouchUpInside: Selector?
-    var actionTouchUp: Selector?
-    var actionTouchDown: Selector?
-    weak var targetTouchUpInside: AnyObject?
-    weak var targetTouchUp: AnyObject?
-    weak var targetTouchDown: AnyObject?
-    
-    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        if (!isEnabled) {
-            return
-        }
+    let touch = touches.first
+    let touchLocation = touch?.location(in: parent!)
+    if (frame.contains(touchLocation!)) {
         isSelected = true
-        if (targetTouchDown != nil && targetTouchDown!.respondsToSelector(actionTouchDown!)) {
-            UIApplication.sharedApplication().sendAction(actionTouchDown!, to: targetTouchDown, from: self, forEvent: nil)
-        }
-    }
-    
-    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        
-        if (!isEnabled) {
-            return
-        }
-        
-        let touch: AnyObject! = touches.first
-        let touchLocation = touch.locationInNode(parent!)
-        
-        if (CGRectContainsPoint(frame, touchLocation)) {
-            isSelected = true
-        } else {
-            isSelected = false
-        }
-        
-    }
-    
-    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        if (!isEnabled) {
-            return
-        }
-        
+    } else {
         isSelected = false
-        
-        if (targetTouchUpInside != nil && targetTouchUpInside!.respondsToSelector(actionTouchUpInside!)) {
-            let touch: AnyObject! = touches.first
-            let touchLocation = touch.locationInNode(parent!)
-            
-            if (CGRectContainsPoint(frame, touchLocation) ) {
-                UIApplication.sharedApplication().sendAction(actionTouchUpInside!, to: targetTouchUpInside, from: self, forEvent: nil)
-            }
-            
-        }
-        
-        if (targetTouchUp != nil && targetTouchUp!.respondsToSelector(actionTouchUp!)) {
-            UIApplication.sharedApplication().sendAction(actionTouchUp!, to: targetTouchUp, from: self, forEvent: nil)
-        }
     }
     
+}
+
+override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+    if (!isEnabled) {
+        return
+    }
+    
+    isSelected = false
+    
+    if (targetTouchUpInside != nil && targetTouchUpInside!.responds(to: actionTouchUpInside!)) {
+        let touch  = touches.first
+        let touchLocation = touch?.location(in: parent!)
+        
+        if (frame.contains(touchLocation!) ) {
+            UIApplication.shared.sendAction(actionTouchUpInside!, to: targetTouchUpInside, from: self, for: nil)
+        }
+        
+    }
+    
+    if (targetTouchUp != nil && targetTouchUp!.responds(to: actionTouchUp!)) {
+        UIApplication.shared.sendAction(actionTouchUp!, to: targetTouchUp, from: self, for: nil)
+    }
+}
 }
